a7c4cdf46904668b36e8c777c33318cecea517fa
Vlad <alzwded@gmail.com>
2015-12-01 14:25:42 +0200

    can read microphone
    thanks you internet, wherever you are
    
    you are here: https://subvisual.co/blog/posts/39-tutorial-html-audio-capture-streaming-to-node-js-no-browser-extensions

--- client.js
+++ client.js
@@ -24,4 +24,38 @@ function button_onclick(e)
 
 function init_voip()
 {
+    var session = {
+audio: true,
+       video: false
+    }
+    var recordRTC = null
+    navigator.webkitGetUserMedia(session, initializeRecorder, onError)
+}
+
+function initializeRecorder(stream) {
+    var audioContext = window.AudioContext
+    var context = new AudioContext()
+    var audioInput = context.createMediaStreamSource(stream)
+    var bufferSize = 2048
+    // create a javascript node
+    var recorder = context.createScriptProcessor(bufferSize, 1, 1)
+    // specify the processing function
+    recorder.onaudioprocess = recorderProcess
+    // connect stream to our recorder
+    audioInput.connect(recorder)
+    // connect our recorder to the previous destination
+    recorder.connect(context.destination)
+}
+
+function recorderProcess(e) {
+    var samp = e.inputBuffer.getChannelData(0)
+    for(var i = 0; i < e.inputBuffer.length; i++) {
+        if(Math.abs(samp[i]) > 0.7) {
+            console.log("big sample")
+        }
+    }
+}
+
+function onError(e) {
+    alert('cannot microphone')
 }
